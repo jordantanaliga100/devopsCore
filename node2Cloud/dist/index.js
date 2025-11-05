f5f4c05429c3ecd13b854d26bd4fb5a729ae48df
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-11-05 17:39:33 +0800

    hotfix(dist): removed build files

--- node2Cloud/dist/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-const env = process.env.NODE_ENV || 'local';
-const envFile = env === 'prod' ? '.env.prod' : '.env.local';
-dotenv.config({
-    path: path.resolve(process.cwd(), envFile),
-});
-import './server';
-//# sourceMappingURL=index.js.map
--- node2Cloud/middlewares/Top.d.ts
+++
@@ -1,3 +0,0 @@
-import { RequestHandler } from 'express';
-export declare const TopMiddlewares: RequestHandler[];
-//# sourceMappingURL=Top.d.ts.map
