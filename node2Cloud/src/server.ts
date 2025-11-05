a8e066c15c14f8d88c5b2025c360c77f3c8ab556
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-11-05 16:29:40 +0800

    chore(setups): bootstrapped server

---
+++ node2Cloud/src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+
+const PORT = Number(process.env.PORT) || 5000;
+const startServer = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server Alive: http://localhost:${PORT} 🚀 `);
+    });
+  } catch (error) {
+    console.log(`Server Error: ${error}`);
+  }
+};
+
+startServer();
