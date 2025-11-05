dee3298b5fd1d74ed11642400f1f5a0e54e651b1
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-11-06 00:46:43 +0800

    fix(logger): tweak logger

--- node2Cloud/helpers/logger.ts
+++ node2Cloud/helpers/logger.ts
@@ -1,19 +1,15 @@
 import winston from 'winston'
 
 export const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  level: process.env.LOG_LEVEL!,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'aquisitions-api' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or higher to `error.log`
-    //   (i.e., error, fatal, but not other levels)
-    //
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    //info` or higher to `combined.log`
-    // - Write all logs with importance level of `
-    //   (i.e., fatal, error, warn, and info, but not trace)
-    //
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 })
@@ -25,7 +21,7 @@ export const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
     })
   )
 }
--- node2Cloud/src/app.ts
+++ node2Cloud/src/app.ts
@@ -2,15 +2,25 @@ import type { Express, Request, Response } from 'express'
 import express from 'express'
 import { BottomMiddlewares } from '../middlewares/Bottom.js'
 import { TopMiddlewares } from '../middlewares/Top.js'
+import { users } from '../models/schema.js'
+
 const app: Express = express()
 
 TopMiddlewares.forEach(mw => app.use(mw))
 
-app.get('/', (req: Request, res: Response) => {
-  const db = req.app.locals.db;
+app.get('/', async (req: Request, res: Response) => {
+  const db = req.app.locals.db
 
-  const data = db.select().from('users')
-  res.json({msg:`Alive 🚀`, data: data})
+  const data = await db.select().from(users)
+  res.json({ msg: `Alive 🚀`, data: data })
+})
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  })
 })
 
 BottomMiddlewares.forEach(mw => app.use(mw))
